31f76ea7994c55f7317026d00e00980de532440c
Vichel <2117236210@qq.com>
2025-02-21 12:18:19 +0800

    完成拼图游戏

--- 拼图游戏/script/index.js
+++ 拼图游戏/script/index.js
@@ -1,195 +1,183 @@
-/**
- * 游戏配置
- */
-var gameConfig = {
-    width: 500,
-    height: 500,
-    rows: 3, //行数
-    cols: 3, //列数
-    isOver: false, //游戏是否结束
-    imgurl: "img/lol.png", //图片路径，注意：相对的是页面路径
-    dom: document.getElementById("game") //游戏的dom对象
+const gameConfig = {
+  width: 500,
+  height: 500,
+  dom: document.querySelector("#game"),
+  imgSrc: "./img/lol.png",
+  columns: 3,
+  rows: 3,
+  isGameOver: false,
 };
-//每一个小块的宽高
-gameConfig.pieceWidth = gameConfig.width / gameConfig.cols;
+
+gameConfig.pieceWidth = gameConfig.width / gameConfig.columns;
 gameConfig.pieceHeight = gameConfig.height / gameConfig.rows;
-//小块的数量
-gameConfig.pieceNumber = gameConfig.rows * gameConfig.cols;
 
-var blocks = []; //包含小方块信息的数组
-
-function isEqual(n1, n2) {
-    return parseInt(n1) === parseInt(n2);
-}
-
-/**
- * 小方块的构造函数
- * @param {*} left 
- * @param {*} top 
- * @param {*} isVisible 是否可见
- */
-function Block(left, top, isVisible) {
-    this.left = left; //当前的横坐标
-    this.top = top; //当前的纵坐标
-    this.correctLeft = this.left; //正确的横坐标
-    this.correctTop = this.top; //正确的纵坐标
-    this.isVisible = isVisible; //是否可见
+class Block {
+  constructor(left, top, visible) {
+    this.trueLeft = left;
+    this.trueTop = top;
+    this.left = left;
+    this.top = top;
+    this.visible = visible;
     this.dom = document.createElement("div");
+    this.dom.style.position = "absolute";
     this.dom.style.width = gameConfig.pieceWidth + "px";
     this.dom.style.height = gameConfig.pieceHeight + "px";
-    this.dom.style.background = `url("${gameConfig.imgurl}") -${this.correctLeft}px -${this.correctTop}px`;
-    this.dom.style.position = "absolute";
     this.dom.style.border = "1px solid #fff";
     this.dom.style.boxSizing = "border-box";
-    this.dom.style.cursor = "pointer";
-    // this.dom.style.transition = ".5s"; //css属性变化的时候，在0.5秒中内完成
-    if (!isVisible) {
-        this.dom.style.display = "none";
+    this.dom.style.background = `url(${gameConfig.imgSrc}) -${this.trueLeft}px -${this.trueTop}px`;
+    if (!this.visible) {
+      this.dom.style.display = "none";
     }
     gameConfig.dom.appendChild(this.dom);
-
-    this.show = function () {
-        //根据当前的left、top，重新设置div的位置
-        this.dom.style.left = this.left + "px";
-        this.dom.style.top = this.top + "px";
-    }
-    //判断当前方块是否在正确的位置上
-    this.isCorrect = function () {
-        return isEqual(this.left, this.correctLeft) && isEqual(this.top, this.correctTop);
-    }
-
     this.show();
+  }
+
+  /**
+   * 根据当前的left、top值更新dom的位置
+   */
+  show() {
+    this.dom.style.left = this.left + "px";
+    this.dom.style.top = this.top + "px";
+  }
+
+  isRight() {
+    return (
+      this.trueLeft === this.left &&
+      this.trueTop === this.top
+    );
+
+  }
 }
 
-/**
- * 初始化游戏
- */
+const blocks = [];
+
 function init() {
-    //1. 初始化游戏的容器
-    initGameDom();
-    //2. 初始化小方块
-    //2.1 准备好一个数组，数组的每一项是一个对象，记录了每一个小方块的信息
-    initBlocksArray();
-    //2.2 数组洗牌
-    shuffle();
-    //3. 注册点击事件
-    regEvent();
+  // 1.初始化游戏容器
+  initGameContainer();
+  // 2.初始化小方块
+  // 2.1 创建小方块
+  createBlocks();
+  // 2.2 随机排序
+  shuffle();
+  // 3. 注册事件
+  regEvent();
 
-    /**
-     * 处理点击事件
-     */
-    function regEvent() {
-        //找到看不见的方块
-        var inVisibleBlock = blocks.find(function (b) {
-            return !b.isVisible;
-        });
-        blocks.forEach(function (b) {
-            b.dom.onclick = function () {
-                if (gameConfig.isOver) {
-                    return;
-                }
-                //判断是可以交换
-                if (b.top === inVisibleBlock.top &&
-                    isEqual(Math.abs(b.left - inVisibleBlock.left), gameConfig.pieceWidth)
-                    ||
-                    b.left === inVisibleBlock.left &&
-                    isEqual(Math.abs(b.top - inVisibleBlock.top), gameConfig.pieceHeight)) {
-                    //交换当前方块和看不见的方块的坐标位置
-                    exchange(b, inVisibleBlock);
-                    //游戏结束判定
-                    isWin();
-                }
+  /**
+   * 初始化游戏容器
+   * 设置宽高、边框、定位等
+   */
+  function initGameContainer() {
+    gameConfig.dom.style.width = gameConfig.width + "px";
+    gameConfig.dom.style.height = gameConfig.height + "px";
+    gameConfig.dom.style.border = "2px solid #ccc";
+    gameConfig.dom.style.position = "relative";
+  }
 
-                //交换当前方块和看不见的方块的坐标位置
-                // exchange(b, inVisibleBlock);
-                // //游戏结束判定
-                // isWin();
-            }
-        })
+  /**
+   * 创建小方块数组
+   * 按行、列循环依次创建每个小方块
+   */
+  function createBlocks() {
+    for (let i = 0; i < gameConfig.rows; i++) {
+      for (let j = 0; j < gameConfig.columns; j++) {
+        // 创建小方块
+        const visible =
+          i === gameConfig.rows - 1 && j === gameConfig.columns - 1
+            ? false
+            : true;
+        const b = new Block(
+          j * gameConfig.pieceWidth,
+          i * gameConfig.pieceHeight,
+          visible
+        );
+        // 将小方块添加到数组中
+        blocks.push(b);
+      }
     }
+  }
 
-    /**
-     * 游戏结束判定
-     */
-    function isWin() {
-        var wrongs = blocks.filter(function (item) {
-            return !item.isCorrect();
-        });
-        if (wrongs.length === 0) {
-            gameConfig.isOver = true;
-            //游戏结束，去掉所有边框
-            blocks.forEach(function (b) {
-                b.dom.style.border = "none";
-                b.dom.style.display = "block";
-            });
+  /**
+   * 打乱小方块的位置
+   * 遍历每个小方块(除了最后一个看不到的)，和随机索引的小方块交换位置
+   */
+  function shuffle() {
+    blocks.forEach((block) => {
+      if (block.visible) {
+        const randomIndex = getRandomIndex(0, blocks.length - 1);
+        exchange(block, blocks[randomIndex]);
+      }
+    });
+  }
+
+  /**
+   * 交换两个小方块的位置
+   * @param {Block} b1
+   * @param {Block} b2
+   */
+  function exchange(b1, b2) {
+    let temp = b1.left;
+    b1.left = b2.left;
+    b2.left = temp;
+
+    temp = b1.top;
+    b1.top = b2.top;
+    b2.top = temp;
+
+    b1.show();
+    b2.show();
+  }
+
+  /**
+   * 得到随机索引
+   * 假设希望得到[0,8),传0,8
+   */
+  function getRandomIndex(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  /**
+   * 注册事件
+   */
+  function regEvent() {
+    const emptyBlock = blocks.find((block) => !block.visible);
+    blocks.forEach((block) => {
+      block.dom.onclick = function () {
+        if(canExchange(block,emptyBlock)) {
+          exchange(block,emptyBlock);
+          isWin();
         }
+      };
+    });
+  }
+
+  /**
+   * 判断当前小方块是否可以交换
+   */
+  function canExchange(b, emptyB) {
+    if (b === emptyB) {
+      return false;
     }
-
-    /**
-     * 随机数，能取到最大值
-     * @param {*} min 
-     * @param {*} max 
-     */
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max + 1 - min) + min);
+    if (
+      (b.top === emptyB.top &&
+        Math.abs(b.left - emptyB.left) === gameConfig.pieceWidth) ||
+      (b.left === emptyB.left &&
+        Math.abs(b.top - emptyB.top) === gameConfig.pieceHeight)
+    ) {
+      return true;
     }
+    return false;
+  }
 
-    /**
-     * 交换两个方块的left和top
-     * @param {*} b1 
-     * @param {*} b2 
-     */
-    function exchange(b1, b2) {
-        var temp = b1.left;
-        b1.left = b2.left;
-        b2.left = temp;
-
-        temp = b1.top;
-        b1.top = b2.top;
-        b2.top = temp;
-
-        b1.show();
-        b2.show();
+  /**
+   * 判断是否游戏结束
+   * 结束即：每个方块当前的位置和真实位置相等
+   */
+  function isWin() {
+    const rightBlocks = blocks.filter((block) => block.isRight());
+    if (rightBlocks.length === blocks.length) {
+      alert("游戏结束");
     }
-
-    /**
-     * 给blocks数组洗牌
-     */
-    function shuffle() {
-        for (var i = 0; i < blocks.length - 1; i++) {
-            //随机产生一个下标
-            var index = getRandom(0, blocks.length - 2);
-            //将数组的当前项与随机项交换left和top值
-            exchange(blocks[i], blocks[index]);
-        }
-    }
-
-    /**
-     * 初始化一个小方块的数组
-     */
-    function initBlocksArray() {
-        for (var i = 0; i < gameConfig.rows; i++) {
-            for (var j = 0; j < gameConfig.cols; j++) {
-                //i 行号，j 列号
-                var isVisible = true;
-                if (i === gameConfig.rows - 1 && j === gameConfig.cols - 1) {
-                    isVisible = false;
-                }
-                var b = new Block(j * gameConfig.pieceWidth, i * gameConfig.pieceHeight, isVisible);
-                blocks.push(b);
-            }
-        }
-    }
-
-    /**
-     * 初始化游戏容器
-     */
-    function initGameDom() {
-        gameConfig.dom.style.width = gameConfig.width + "px";
-        gameConfig.dom.style.height = gameConfig.height + "px";
-        gameConfig.dom.style.border = "2px solid #ccc";
-        gameConfig.dom.style.position = "relative";
-    }
+  }
 }
 
 init();
